611f61b81d6b18d990d80381bcd56e92cc5ce384
Arthur Eigenbrot <aeigenbrot@nso.edu>
2019-08-01 11:30:51 -0400

    First stab at non-hardcoded periods
    Total overkill to use poliastro at this point, but it opens the door for
    potentially exciting features in the future

--- astro_cakeday/planets.py
+++ astro_cakeday/planets.py
@@ -1,12 +1,21 @@
+import astropy.units as u
+from astropy.time import Time
+from poliastro.twobody import Orbit
+from poliastro.bodies import Mercury, Venus, Earth, Mars, Saturn, Jupiter, Neptune, Uranus, Pluto
 
 
+class Planets():
 
-planet_lengths = {'Mercury':87.969, 
-                  'Venus':224.701, 
-                  'Earth':365,
-                  'Mars':686.980, 
-                  'Jupiter':4332.589, 
-                  'Saturn':10759.22, 
-                  'Uranus':30685.4, 
-                  'Neptune':60189, 
-                  'Pluto':90560}
+    def __init__(self, epoch):
+
+        if type(epoch) is not Time:
+            epoch = Time(epoch)
+
+        self.orbits = {}
+        self.periods = {}
+        for name, body in zip(['Mercury', 'Venus', 'Earth', 'Mars', 'Saturn', 'Jupiter', 'Neptune', 'Uranus'],
+                              [Mercury, Venus, Earth, Mars, Saturn, Jupiter, Neptune, Uranus]):
+            orbit = Orbit.from_body_ephem(body, epoch)
+            self.orbits[name] = orbit
+            self.periods[name] = orbit.period.to(u.d)
+

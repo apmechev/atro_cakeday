548a32ab6591c681a2d5beaec8f787f5aae0a3ce
apmechev <apmechev@gmail.com>
2019-08-01 12:07:01 -0400

    name is passed, date is not

--- astro_cakeday/__init__.py
+++ astro_cakeday/__init__.py
@@ -2,20 +2,32 @@ import os
 import pdb
 
 from flask import Flask
+from flask import request
 from flask import render_template 
 from flask import flash
 from flask_datepicker import datepicker
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
-from wtforms import StringField
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms import SubmitField
 from wtforms.fields import DateField
+from wtforms.validators import DataRequired
 
 from astro_cakeday.populate_cal import populate_ical
 
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    birthdate = DateField(id='birthday')
+    remember_me = BooleanField('Remember Me')
+    submit = SubmitField('Sign In')
+
+
+
 class MyForm(FlaskForm):
     name =  StringField('Your Name')
-#    birthdate = DateField(id='birthday')
+    birthdate = DateField(id='.dp')
     submit = SubmitField('Give me my birthday:)')
 
 def create_app(test_config=None):
@@ -42,13 +54,20 @@ def create_app(test_config=None):
     except OSError:
         pass
 
-    # a simple page that says hello
     @app.route('/',  methods=['GET', 'POST'])
     def hello():
         form = MyForm()
-        if form.validate_on_submit():
-            populate_ical(person_name=form.name.data,  birthday=form.date.data)
-            return  
+        if request.method == 'POST':
+            flash('user {}, birthday={}'.format(
+            form.name.data, form.birthdate.data))
+#            populate_ical(person_name=form.name.data,  birthday=form.date.data)
+            return  render_template('test.html', form=form)
+
         return render_template('birthday.html', form=form)
-  
+
+    @app.route('/login')
+    def login():
+        form = LoginForm()
+        return render_template('login.html', title='Sign In', form=form)
+
     return app

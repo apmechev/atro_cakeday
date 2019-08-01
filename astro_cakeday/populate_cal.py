7a50919b7a32591c369e3469c817b487438a5c48
Cassidy Wagner <cassidymwagner@gmail.com>
2019-08-01 09:38:48 -0500

    added loop over number of desired birthdays

--- astro_cakeday/populate_cal.py
+++ astro_cakeday/populate_cal.py
@@ -4,14 +4,14 @@ from datetime import datetime, timedelta
 from icalendar import Calendar, Event 
 
 birthday_event = datetime.strptime("1989-06-21","%Y-%m-%d")
+birthday_number = 3
 
 cal = Calendar()
 
-for i in planet_lengths:
-    print(i)
-
-uranus_1 = PlanetaryBirthday("Uranus", 1)
-uranus_1.add('dtstart', birthday_event + timedelta(planet_lengths['Uranus']))
-uranus_1.add('dtend', birthday_event + timedelta(planet_lengths['Uranus']+1))
-
-cal.add_component(uranus_1)
+for number in range(0,birthday_number):
+    for name in planet_lengths:
+        planet = PlanetaryBirthday(str(name), number)
+        planet.add('dtstart', birthday_event + timedelta(planet_lengths[name]))
+        planet.add('dtend', birthday_event + timedelta(planet_lengths[name]+1))
+        
+        cal.add_component(planet)

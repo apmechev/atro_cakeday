aa0acfd98d38ff094458d9482dc38426a06b3a11
apmechev <apmechev@gmail.com>
2019-08-01 11:43:44 -0400

    adding not working copy of forms

--- astro_cakeday/__init__.py
+++ astro_cakeday/__init__.py
@@ -1,14 +1,22 @@
 import os
+import pdb
 
 from flask import Flask
 from flask import render_template 
+from flask import flash
 from flask_datepicker import datepicker
 from flask_bootstrap import Bootstrap
-from flask_wtf import Form
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms import SubmitField
 from wtforms.fields import DateField
 
-class MyForm(Form):
-    date = DateField(id='datepick')
+from astro_cakeday.populate_cal import populate_ical
+
+class MyForm(FlaskForm):
+    name =  StringField('Your Name')
+#    birthdate = DateField(id='birthday')
+    submit = SubmitField('Give me my birthday:)')
 
 def create_app(test_config=None):
     # create and configure the app
@@ -16,6 +24,9 @@ def create_app(test_config=None):
     app.config.from_mapping(
         SECRET_KEY='sdafsadfsa',
     )
+    UPLOAD_FOLDER = 'uploads'
+    app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
     datepicker(app)
     Bootstrap(app)
     if test_config is None:
@@ -32,9 +43,12 @@ def create_app(test_config=None):
         pass
 
     # a simple page that says hello
-    @app.route('/')
+    @app.route('/',  methods=['GET', 'POST'])
     def hello():
         form = MyForm()
+        if form.validate_on_submit():
+            populate_ical(person_name=form.name.data,  birthday=form.date.data)
+            return  
         return render_template('birthday.html', form=form)
-
+  
     return app

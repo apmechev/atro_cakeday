1b8d3258485545254fcb5d5599cd1228bf9ff1d8
apmechev <apmechev@gmail.com>
2019-08-01 09:57:43 -0400

    adding planet length in days

---
+++ astro_cakeday/planets.py
@@ -0,0 +1,12 @@
+
+
+
+planet_lenghts = {'Mercury':87.969, 
+                  'Venus':224.701, 
+                  'Earth':365,
+                  'Mars':686.980, 
+                  'Jupiter':4332.589, 
+                  'Saturn':10759.22, 
+                  'Uranus':30685.4, 
+                  'Neptune':60189, 
+                  'Pluto':90560}

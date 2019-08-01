05bf4e850d8bd4e7f04cfcb67b9f0e78d6b2709a
Arthur Eigenbrot <aeigenbrot@nso.edu>
2019-08-01 13:39:53 -0400

    Added sparser outputs for inferior planets. Event is now just a day.
    The cadence of birthday skips is defined in plants.PLANET_DB

--- astro_cakeday/planets.py
+++ astro_cakeday/planets.py
@@ -5,12 +5,23 @@ from poliastro import bodies
 
 PLANETS_TO_USE = ['Mercury', 'Venus', 'Earth', 'Mars', 'Saturn', 'Jupiter', 'Neptune', 'Uranus']
 
+PLANET_DB = {'Mercury': 15,
+             'Venus': 5,
+             'Earth': 1,
+             'Mars': 1,
+             'Saturn': 1,
+             'Jupiter': 1,
+             'Neptune': 1,
+             'Uranus': 1}
+
 class Planets():
 
     def __init__(self, epoch):
 
         if type(epoch) is not Time:
-            epoch = Time(epoch)
+            self.epoch = Time(epoch)
+        else:
+            self.epoch = epoch
 
         self.orbits = {}
         self.periods = {}
@@ -23,7 +34,13 @@ class Planets():
             except AttributeError:
                 raise AttributeError('Currently do not have data for {}'.format(planet))
 
-            orbit = Orbit.from_body_ephem(body, epoch)
+            orbit = Orbit.from_body_ephem(body, self.epoch)
             self.orbits[planet] = orbit
             self.periods[planet] = orbit.period.to(u.d)
+
+    def get_birthday(self, planet, birthday_number=1):
+
+        birthday = self.epoch + self.periods[planet] * birthday_number * PLANET_DB[planet]
+
+        return birthday
 

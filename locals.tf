8fb89afdc937ff312ae8b74999b46144b7c45d17
Alexandar Mechev <apmechev@gmail.com>
2021-06-19 12:46:06 +0200

    fix typo

--- locals.tf
+++ locals.tf
@@ -2,5 +2,5 @@ locals {
     prefix = "test-"
     bakery_bucket_name = "${local.prefix}${var.bakery_bucket_prefix}.${var.site_name}"
     frontend_bucket_name = "${local.prefix}${var.site_name}"
-    lambda_function_name = "${local.prefix}cakedyas_space_process"
+    lambda_function_name = "${local.prefix}cakedays_space_process"
 }

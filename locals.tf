da4d45342073c526b6f1c5b9702059223f7ae41f
Alexandar Mechev <apmechev@gmail.com>
2021-07-20 21:08:50 +0200

    fix site name, and branch

--- locals.tf
+++ locals.tf
@@ -1,7 +1,6 @@
 locals {
-  prefix               = var.branch_name != "master" ? "${replace(var.branch_name, "/\\W|_|\\s/", "-")}-" : ""
-  safe_site_name       = replace(var.site_name, "/\\W|_|\\s/", "-")  #https://stackoverflow.com/a/60734389
-  bakery_bucket_name   = "${local.prefix}${var.bakery_bucket_prefix}-${local.safe_site_name}"
-  frontend_bucket_name = "${local.prefix}${local.safe_site_name}"
+  prefix               = var.branch_name != "master" ? "${replace(var.branch_name"/\\W|_|\\s/", "-")}-" : ""  #https://stackoverflow.com/a/60734389
+  bakery_bucket_name   = "${local.prefix}${var.bakery_bucket_prefix}-${var.site_name}"
+  frontend_bucket_name = "${local.prefix}${var.site_name}"
   lambda_function_name = "${local.prefix}cakedays_space_process"
 }

81c5f2dab1bfe9c5993619969b6b79c59f5e9c67
Alexandar Mechev <apmechev@gmail.com>
2021-07-20 21:03:08 +0200

    x

--- locals.tf
+++ locals.tf
@@ -1,5 +1,5 @@
 locals {
-  prefix               = var.branch_name != "master" ? "${var.branch_name}-" : ""
+  prefix               = var.branch_name != "master" ? "${replace(var.branch_name, "/\\W|_|\\s/", "-")}-" : ""
   safe_site_name       = replace(var.site_name, "/\\W|_|\\s/", "-")  #https://stackoverflow.com/a/60734389
   bakery_bucket_name   = "${local.prefix}${var.bakery_bucket_prefix}-${local.safe_site_name}"
   frontend_bucket_name = "${local.prefix}${local.safe_site_name}"

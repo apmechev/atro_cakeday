d69c7338a85c5da6bdc87b317639ac217c34395f
Alexandar Mechev <apmechev@gmail.com>
2021-07-20 20:53:13 +0200

    add space too

--- locals.tf
+++ locals.tf
@@ -1,6 +1,6 @@
 locals {
   prefix               = var.branch_name != "master" ? "${var.branch_name}-" : ""
-  safe_site_name       = replace(var.site_name, "/\._/", "-")
+  safe_site_name       = replace(var.site_name, "/\\._ /", "-")
   bakery_bucket_name   = "${local.prefix}${var.bakery_bucket_prefix}-${local.safe_site_name}"
   frontend_bucket_name = "${local.prefix}${local.safe_site_name}"
   lambda_function_name = "${local.prefix}cakedays_space_process"

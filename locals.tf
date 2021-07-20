9ffe99e709a2226bdd31e9d51322e557ba1d1244
Alexandar Mechev <apmechev@gmail.com>
2021-07-20 20:47:10 +0200

    var2local

--- locals.tf
+++ locals.tf
@@ -1,7 +1,7 @@
 locals {
   prefix               = var.branch_name != "master" ? "${var.branch_name}-" : ""
   safe_site_name       = replace(var.site_name, ".", "-")
-  bakery_bucket_name   = "${local.prefix}${var.bakery_bucket_prefix}-${var.safe_site_name}"
-  frontend_bucket_name = "${local.prefix}${var.safe_site_name}"
+  bakery_bucket_name   = "${local.prefix}${var.bakery_bucket_prefix}-${local.safe_site_name}"
+  frontend_bucket_name = "${local.prefix}${local.safe_site_name}"
   lambda_function_name = "${local.prefix}cakedays_space_process"
 }

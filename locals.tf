11e64ceb0f8ec43a60040ca681de8dddeb5ab60f
Alexandar Mechev <apmechev@gmail.com>
2021-07-20 18:51:18 +0200

    locals: fix branch name

--- locals.tf
+++ locals.tf
@@ -1,5 +1,5 @@
 locals {
-  prefix               = var.branch != "master" ? concat(var.branch, "-") : ""
+  prefix               = var.branch_name != "master" ? concat(var.branch_name, "-") : ""
   bakery_bucket_name   = "${local.prefix}${var.bakery_bucket_prefix}.${var.site_name}"
   frontend_bucket_name = "${local.prefix}${var.site_name}"
   lambda_function_name = "${local.prefix}cakedays_space_process"

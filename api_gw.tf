addbfda87af5e979bc73b98bd67528f8b4c70343
Alexandar Mechev <apmechev@gmail.com>
2021-07-22 15:43:11 +0200

    typos

--- api_gw.tf
+++ api_gw.tf
@@ -42,7 +42,7 @@ resource "aws_apigatewayv2_route" "bake_cake" {
 resource "aws_apigatewayv2_integration" "bake_cake" {
   api_id = aws_apigatewayv2_api.submit_cake.id
 
-  integration_uri    = mdoule.process_lambda.invoke_arn
+  integration_uri    = module.process_lambda.invoke_arn
   integration_type   = "AWS_PROXY"
   integration_method = "POST"
 }
@@ -50,7 +50,7 @@ resource "aws_apigatewayv2_integration" "bake_cake" {
 resource "aws_lambda_permission" "api_gw" {
   statement_id  = "AllowExecutionFromAPIGateway"
   action        = "lambda:InvokeFunction"
-  function_name = mdoule.process_lambda.name
+  function_name = module.process_lambda.name
   principal     = "apigateway.amazonaws.com"
 
   source_arn = "${aws_apigatewayv2_api.submit_cake.execution_arn}/*/*"

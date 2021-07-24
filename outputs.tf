f6298e2d1c6e4abd7879f746df401cf1781b4d42
Alexandar Mechev <apmechev@gmail.com>
2021-07-24 13:10:37 +0200

    outputs: add s3_website_url as an output

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,8 @@
 output "api_gateway_uri" {
   value = "${aws_apigatewayv2_api.submit_cake.api_endpoint}/${local.submit_stage_name}/bake"
 }
+
+
+output "s3_website_url" {
+  value = aws_s3_bucket.site_bucket.website_domain
+}
